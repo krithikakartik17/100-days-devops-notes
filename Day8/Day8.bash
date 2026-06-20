04b72ccda2597a3de3cde28971a24f7633d71775
krithikakartik17 <krithikakartik17@gmail.com>
2026-06-20 19:53:52 +0530

    Day 8

---
+++ Day8/Day8.bash
@@ -0,0 +1,9 @@
+# switch to root user
+sudo su -
+
+#check if pip3 is installed
+pip3 --version
+
+# install ansible using pip3 (version as specified in the challenge)
+pip3 install ansible==4.8.0
+
