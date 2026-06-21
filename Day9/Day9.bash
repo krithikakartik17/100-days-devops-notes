1ab94bc14faa8bac4100c8105e84dba8aed4c981
krithikakartik17 <krithikakartik17@gmail.com>
2026-06-21 17:52:19 +0530

    Day 9

---
+++ Day9/Day9.bash
@@ -0,0 +1,49 @@
+# login to the database server using Secure shell
+ssh peter@stdb01
+
+# check if mariadb is installed
+mariadb --version
+
+# Trying to restart mariadb service
+sudo systemctl restart mariadb
+
+# check mariadb status
+sudo systemctl status mariadb
+
+# status shows exit code as 1
+# view the detailed error logs
+sudo journalctl -xeu mariadb.service
+
+ls /var/log/mariadb
+# permission denied
+
+# switch to root user
+sudo su -
+
+# list the contents inside the directory
+ls /var/log/mariadb
+
+# change to this directory
+cd /var/log/mariadb
+
+# list the contents of the directory
+ls
+
+# use cat command to open the contents of the file
+sudo cat mariadb.log
+
+# it will show an error like this
+# [ERROR] mariadbd: Can't create/write to file '/run/mariadb/mariadb.pid' (Errcode: 13 "Permission denied")
+
+# to view the permissions of the /run/mariadb directory
+ls -l /run/mariadb
+
+# change mode to ugo+rwx for the directory
+sudo chmod 777 /run/mariadb
+
+# restart mariadb service
+sudo systemctl restart mariadb
+
+# verify the status of mariadb service, it should show status as running
+sudo systemctl status mariadb
+
